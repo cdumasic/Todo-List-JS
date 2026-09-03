9f98bb5f0cd334df275a105c1475750d5064e425
cdumasic <cumasic@unsa.edu.pe>
2026-09-02 23:10:45 -0500

    style: changes px

--- style_atributes.js
+++ style_atributes.js
@@ -1,8 +1,8 @@
 export const configEstilosBotones = {
-    'Comenzando':'text-white bg-green-500 hover:bg-green-400 px-2 py-1 mx-1 rounded-md font-medium shadow-sm transition-all active:scale-95 cursor-pointer',
-    'Completado':'text-white bg-gray-500 hover:bg-gray-400 px-2 py-1 mx-1 rounded-md font-medium shadow-sm transition-all active:scale-95 cursor-pointer',
-    'Deshacer':'text-white bg-red-400 hover:bg-red-300 px-2 py-1 mx-1 rounded-md font-medium shadow-sm transition-all active:scale-95 cursor-pointer',
-    'Eliminar':'text-black bg-red-600 hover:bg-red-500 px-2 py-1 mx-1 rounded-md font-medium shadow-sm transition-all active:scale-95 cursor-pointer' 
+    'Comenzando':'text-white bg-green-500 hover:bg-green-400 px-1 py-1 mx-1 rounded-md font-medium shadow-sm transition-all active:scale-95 cursor-pointer',
+    'Completado':'text-white bg-gray-500 hover:bg-gray-400 px-1 py-1 mx-1 rounded-md font-medium shadow-sm transition-all active:scale-95 cursor-pointer',
+    'Deshacer':'text-white bg-red-400 hover:bg-red-300 px-1 py-1 mx-1 rounded-md font-medium shadow-sm transition-all active:scale-95 cursor-pointer',
+    'Eliminar':'text-black bg-red-600 hover:bg-red-500 px-1 py-1 mx-1 rounded-md font-medium shadow-sm transition-all active:scale-95 cursor-pointer' 
 }
 
 export const configEstiloUItarea = {
